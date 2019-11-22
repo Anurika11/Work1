e32ac67f5d243764ab7873d7c6bba687e8a2a2b4
unknown <AI66@live.mdx.ac.uk>
2019-11-22 16:30:42 +0100

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
-<template>
+<template style="background-image: url('src/assets/flowers.jpg')">
   <div class="jumbotron" style="background-color:#808080;">
     <div class="container">
-      <h1 class="display-4">Hey there!</h1>
+      <h1 class="display-4">Student</h1>
       <p class="lead">We offer awesome extra curricular exercises and classes</p>
       <hr class="my-4">
       <p>Having issues being your best? join to appreciate a wide scope of extra curricular class and exercises intended to make you the best.</p>
-      <router-link class="btn btn-primary btn-lg" to="/signup" role="button">Sign up</router-link>
+      <router-link class="btn btn-primary btn-lg" to="/signup" role="button">Sign in</router-link>
     </div>
   </div>
 </template>
