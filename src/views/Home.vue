473a23c70b0ca91be9eca8658f30ab728033867d
unknown <AI66@live.mdx.ac.uk>
2019-11-22 15:47:05 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Hey there!</h1>
+      <p class="lead">We offer awesome extra curricular exercises and classes</p>
+      <hr class="my-4">
+      <p>Having issues being your best? join to appreciate a wide scope of extra curricular class and exercises intended to make you the best.</p>
+      <router-link class="btn btn-primary btn-lg" to="/signup" role="button">Sign up</router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  filterResutl(){
+
+  }
+}
+</script>
